8c2bec8e5892d1d6dc617f81ef886f3ccc58172e
SCP-079 <scp-079@archlinux.localdomain>
2026-09-13 10:21:12 +0300

    text formatting ting

--- main.go
+++ main.go
@@ -23,14 +23,14 @@ var rootCmd = &cobra.Command{
 	Use:   "cryps [flags] <text>",
 	Short: "Use a defined library to encrypt your text",
 	Long: `A CLI program that takes a user defined encryption library json file and encrypts text using said library
-			Encrypts the given text using the key file passed via -l/--library
-			and prints the textInput to stdout.
+	Encrypts the given text using the key file passed via -l/--library
+	and prints the textInput to stdout.
 
-			Examples:
-			  cryps -l key.json "hello there"
-			  cryps -l key.json -f message.txt
-			  cryps -l key.json -f message.txt -o out.txt
-			  cryps -l key.json -d -f response.txt`,
+	Examples:
+	  cryps -l key.json "hello there"
+	  cryps -l key.json -f message.txt
+	  cryps -l key.json -f message.txt -o out.txt
+	  cryps -l key.json -d -f response.txt`,
 	Args: cobra.ArbitraryArgs,
 	Run:  runCryps,
 }
